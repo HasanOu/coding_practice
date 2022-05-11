e9b8bbbd14b5b233ca2dc8dd2740a9a003e9406c
Hasan Manzour <seyedman@amazon.com>
2022-05-11 09:25:47 -0700

    update

--- array_leetcode.py
+++ array_leetcode.py
@@ -10,8 +10,10 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """
+import collections
 
 import numpy as np
+
 
 def twoSum(nums, target):
     hashmap = dict()
@@ -20,7 +22,7 @@ def twoSum(nums, target):
         hashmap[nums[i]] = i
     for i in range(len(nums)):
         complement = target - nums[i]
-        if complement in hashmap.keys() and hashmap[complement]!= i:
+        if complement in hashmap.keys() and hashmap[complement] != i:
             list_twosum.append(sorted([i, hashmap[complement]]))
 
     list_twosum_ = []
@@ -31,9 +33,9 @@ def twoSum(nums, target):
     return list_twosum_
 
 
-#arr = [1, 5, 4, 4, 7, 9]
-#output = twoSum(arr, 10)
-#print(output)
+# arr = [1, 5, 4, 4, 7, 9]
+# output = twoSum(arr, 10)
+# print(output)
 
 
 """You are given an integer array height of length n.
@@ -58,12 +60,12 @@ def container_with_most_water(arr):
     right = len(arr) - 1
     while left < right:
         width = right - left
-        max_area = max(max_area, width*min(arr[right], arr[left]))
+        max_area = max(max_area, width * min(arr[right], arr[left]))
 
         if arr[left] <= arr[right]:
             left += 1
         else:
-            right -=1
+            right -= 1
 
     return max_area
 
@@ -83,7 +85,7 @@ Output: [[-1,-1,2],[-1,0,1]]
 """
 
 
-def threesum(arr,target):
+def threesum(arr, target):
     hashmap = {}
     threesum_list = []
     for i in np.arange(len(arr)):
@@ -103,9 +105,10 @@ def threesum(arr,target):
     threesum_list_value = [[arr[num] for num in group] for group in threesum_list_]
     return threesum_list_value
 
-#arr = [3,3,1,2,-1,4]
-#output = threesum(arr, 6)
-#print(output)
+
+# arr = [3,3,1,2,-1,4]
+# output = threesum(arr, 6)
+# print(output)
 
 
 """ Given an integer array nums of length n and an integer target, find three 
@@ -126,7 +129,7 @@ def threesum_closest(arr, target):
 
     arr.sort()
     for i in range(len(arr)):
-        lo, hi = i+1, len(arr) - 1
+        lo, hi = i + 1, len(arr) - 1
 
         while (lo < hi):
             sum = arr[i] + arr[lo] + arr[hi]
@@ -141,9 +144,10 @@ def threesum_closest(arr, target):
 
     return target + min_diff
 
-#arr = [-1,4,1,-4]
-#output = threesum_closest(arr, 1)
-#print(output)
+
+# arr = [-1,4,1,-4]
+# output = threesum_closest(arr, 1)
+# print(output)
 
 """Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
 
@@ -157,26 +161,28 @@ Input: nums = [1,0,-1,1,-2,2], target = 0
 Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 """
 
+
 def foursum(arr, target):
     arr.sort()
     list_values = []
     for i in range(len(arr)):
-        for j in np.arange(i+1, len(arr)):
-            lo, hi = j+1, len(arr)-1
+        for j in np.arange(i + 1, len(arr)):
+            lo, hi = j + 1, len(arr) - 1
 
             while lo < hi:
-                sum =  arr[i] + arr[j] + arr[lo] + arr[hi]
+                sum = arr[i] + arr[j] + arr[lo] + arr[hi]
                 if sum == target:
                     list_values.append([arr[i], arr[j], arr[lo], arr[hi]])
 
                 if sum <= target:
-                    lo+=1
+                    lo += 1
                 else:
-                    hi-=1
+                    hi -= 1
 
     return list_values
 
-output = foursum([1,0,-1,1,-2,2], 0)
+
+output = foursum([1, 0, -1, 1, -2, 2], 0)
 print(output)
 
 """
@@ -197,6 +203,7 @@ nums = [0,0,1,1,1,2,2,3,3,4]
 5, nums = [0,1,2,3,4,_,_,_,_,_]
 """
 
+
 def remove_duplicates_from_sorted_array(arr):
     new_array = []
     for item in arr:
@@ -211,7 +218,8 @@ def remove_duplicates_from_sorted_array(arr):
 
     return new_array
 
-arr = [0,0,1,1,1,2,2,3,3,4]
+
+arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 output = remove_duplicates_from_sorted_array(arr)
 print(output)
 
@@ -231,21 +239,23 @@ Given an array of integers nums, find the next permutation of nums.
 The replacement must be in place and use only constant extra memory.
 """
 
+
 def next_permutation(arr):
-    if sorted(arr, reverse=True) == arr:
+    if sorted(arr, reverse = True) == arr:
         return sorted(arr)
 
-    hi = len(arr) -1
-    while arr[hi-1] > arr[hi]:
+    hi = len(arr) - 1
+    while arr[hi - 1] > arr[hi]:
         hi -= 1
 
-    temp = arr[hi-1]
-    arr[hi-1] = arr[hi]
+    temp = arr[hi - 1]
+    arr[hi - 1] = arr[hi]
     arr[hi] = temp
 
     return arr
 
-arr= [2,3,1,4]
+
+arr = [2, 3, 1, 4]
 output = next_permutation(arr)
 print(output)
 
@@ -274,20 +284,21 @@ def search_in_rotated_rorted_array(nums, target):
     begin = 0
     end = len(nums) - 1
     while begin <= end:
-        mid = (begin + end)//2
+        mid = (begin + end) // 2
         if nums[mid] == target:
             return mid
-        if nums[mid] > nums[end]: # Left side of mid is sorted
-            if  nums[begin] <= target and target < nums[mid]: # Target in the left side
+        if nums[mid] > nums[end]:  # Left side of mid is sorted
+            if nums[begin] <= target and target < nums[mid]:  # Target in the left side
                 end = mid - 1
-            else: # in right side
+            else:  # in right side
                 begin = mid + 1
-        else: # Right side is sorted
-            if  nums[mid] < target and target <= nums[end]: # Target in the right side
+        else:  # Right side is sorted
+            if nums[mid] < target and target <= nums[end]:  # Target in the right side
                 begin = mid + 1
-            else: # in left side
+            else:  # in left side
                 end = mid - 1
     return -1
+
 
 arr = [7, 8, 0, 1, 2, 3, 4]
 output = search_in_rotated_rorted_array(arr, 7)
@@ -318,7 +329,6 @@ Output: true
 
 
 def valid_sodoku(arr):
-
     for i in range(9):
         for j in range(9):
             if arr[i][j] == ".":
@@ -336,13 +346,14 @@ def valid_sodoku(arr):
     for i in range(6):
         for j in range(6):
             List = []
-            for x in np.arange(i, i+2):
-                for y in np.arange(j, j+2):
+            for x in np.arange(i, i + 2):
+                for y in np.arange(j, j + 2):
                     item = arr[x][y]
                     if item in List:
                         return False
                     List.append(arr[x][y])
     return True
+
 
 def sodoku1(arr):
     # Use hash set to record the status
@@ -378,18 +389,17 @@ def sodoku1(arr):
     return True
 
 
-arr = [["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","2","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
+arr = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+    , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    , [".", "9", "8", ".", ".", ".", ".", "2", "."]
+    , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 output = sodoku1(arr)
 print(output)
-
 
 """
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
@@ -406,7 +416,7 @@ def Maximum_Subarray(arr):
     temp_sum = 0
     List = []
     for val in arr:
-        temp_sum +=val
+        temp_sum += val
         List = List + [val]
 
         if temp_sum > max_sum:
@@ -419,9 +429,10 @@ def Maximum_Subarray(arr):
 
     return max_sum, best_list
 
-arr = [4,1,2,-1]
-output =  Maximum_Subarray(arr)
-print(output)
+
+# arr = [4, 1, 2, -1]
+# output = Maximum_Subarray(arr)
+# print(output)
 
 """
 Given an array of intervals where intervals[i] = [start_i, end_i], merge all overlapping intervals, 
@@ -431,3 +442,159 @@ Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
 Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 """
+
+
+def merge_interval(arr):
+    arr.sort(key = lambda x: x[0])
+
+    merged = []
+    for item in arr:
+        if not merged or merged[-1][1] < item[0]:
+            merged.append(item)
+        else:
+            merged[-1][1] = max(merged[-1][1], item[1])
+
+    return merged
+
+
+arr = [[0, 4], [5, 7], [2, 3]]
+output = merge_interval(arr)
+print(output)
+
+
+"""
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+"""
+
+
+def sum_k(arr, k):
+    count = 0
+    for start_value in range(len(arr)):
+        sum = 0
+        for end_value in np.arange(start_value, len(arr)):
+            sum += arr[end_value]
+
+            if sum == k:
+                count += 1
+
+    return count
+
+
+arr = [1, 2, 3, 1, 6]
+k = 6
+output = sum_k(arr, k)
+print(output)
+
+"""
+A website domain "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next 
+level, we have "leetcode.com" and at the lowest level, "discuss.leetcode.com". When we visit a domain like 
+"discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" implicitly.
+
+A count-paired domain is a domain that has one of the two formats "rep d1.d2.d3" or "rep d1.d2" where rep is the 
+number of visits to the domain and d1.d2.d3 is the domain itself.
+
+    For example, "9001 discuss.leetcode.com" is a count-paired domain that indicates that discuss.leetcode.com was 
+    visited 9001 times.
+
+Given an array of count-paired domains cpdomains, return an array of the count-paired domains of each subdomain in the 
+input. You may return the answer in any order.
+
+Input: cpdomains = ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+Output: ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+Explanation: We will visit "google.mail.com" 900 times, "yahoo.com" 50 times, "intel.mail.com" once and "wiki.org" 
+5 times.
+For the subdomains, we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
+"""
+
+
+def website_domain(cpdomains):
+    ans = dict()
+    for domain in cpdomains:
+        count, domain = domain.split()
+        count = int(count)
+        frags = domain.split('.')
+        for i in range(len(frags)):
+            key = ".".join(frags[i:])
+            if key in ans.keys():
+                ans[key] += count
+            else:
+                ans[key] = count
+
+    list_ = []
+    for key, value in ans.items():
+        list_.append([value, key])
+
+    return list_
+
+cpdomains = ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+output = website_domain(cpdomains)
+print(output)
+
+
+"""
+A peak element is an element that is strictly greater than its neighbors.
+
+Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return 
+the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+You must write an algorithm that runs in O(log n) time.
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the 
+peak element is 6.
+"""
+
+
+def peak(arr):
+    start = 0
+    end = len(arr) - 1
+    while start < end:
+        mid_point = int((start+end)/2)
+        print(mid_point)
+        if arr[mid_point] >= arr[mid_point-1] and arr[mid_point] >= arr[mid_point+1]:
+            return arr[mid_point]
+        if arr[mid_point] < arr[mid_point+1]:
+            start = mid_point + 1
+        elif arr[mid_point] < arr[mid_point-1]:
+            end = mid_point - 1
+
+    return -1
+
+
+arr = [5, 0, -1, 0, 1, 2]
+output = peak(arr)
+print(output)
+
+
+"""
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+
+"""
+
+
+
