1cdce7fc673e932c2a3fe40caa9fb8e77d5a736f
Hasan Manzour <seyedman@amazon.com>
2022-06-18 12:37:25 -0700

    updated numpy_tricks module

--- numpy_tricks.py
+++ numpy_tricks.py
@@ -17,12 +17,21 @@ import numpy as np
 
 # =========================== Creating an Array ===========================
 a = np.array([1, 2, 3])
+a = np.asarray([1, 2, 3])
+
 a[0] = 5  # Change an element of the array
 print(a)  # Prints "[5, 2, 3]"
+print(2*a)  # Prints "[50, 20, 30]"
+print(2*[5, 2, 3]) # Prints "[5,2,3,5,2,3]"
 
 # Nested sequences, like a list of equal-length lists, will be converted into a multidimen- sional array:
 b = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 print(b[0, 0], b[0, 1], b[1, 0])  # Prints "1 2 5"
+
+arr = [[1, 2, 3, 4], [5, 6, 7, 8]]
+arr2 = np.array(arr)
+print(arr2)  # array([[1, 2, 3, 4],
+                    # [5, 6, 7, 8]])
 
 print(np.array(b))
 print(b.ndim)
@@ -46,6 +55,7 @@ print(c)  # Prints "[[ 7.  7.]
 #          [ 7.  7.]]"
 
 d = np.eye(2)  # Create a 2x2 identity matrix
+d = np.identity(2)  # Create a 2x2 identity matrix
 print(d)  # Prints "[[ 1.  0.]
 #          [ 0.  1.]]"
 
@@ -53,13 +63,18 @@ e = np.random.random((2, 2))  # Create an array filled with random values
 print(e)         # Might print "[[ 0.91940167  0.08143941]
 #               [ 0.68744134  0.87236687]]"
 
+k = np.random.randn(3,4) # might print array([[ 0.16462368,  0.40667506,  0.88176286,  0.73727787],
+                                    #       [ 1.03054928,  1.10905476, -1.41934872, -0.49739245],
+                                    #       [-0.09689096, -1.82724465,  1.19913053, -1.22382975]])
+
 # It’s not safe to assume that np.empty will return an array of all zeros. In many cases,
 # it will return uninitialized garbage values.
 f = np.empty((2, 3, 2))
 print(f)
 
-# =========================== Array indexing ===========================
+print(np.arange(10))    # [0, 1, 2, ..., 9]
 
+# =========================== Array indexing ===========================
 # Slicing: Similar to Python lists, numpy arrays can be sliced. Since arrays may be multidimensional,
 # you must specify a slice for each dimension of the array:
 
@@ -108,6 +123,54 @@ print(col_r2, col_r2.shape)  # Prints "[[ 2]
                              #          [10]] (3, 1)"
 
 # =========================== Integer array indexing: ===========================
+
+# One-dimensional arrays indexing
+
+arr = np.arange(10) # Prints [0,1,...,9]
+print(arr[5])  # Print 5
+print(arr[5:8])  # Print [5,6,7]
+arr[5:8] = 12
+print(arr) # array([ 0, 1, 2, 3, 4, 12, 12, 12, 8, 9])
+
+# An important first dis- tinction from lists is that array slices are views on the original array.
+#  This means that the data is not copied, and any modifications to the view will be reflected in the source array:
+
+arr = np.arange(10) # Prints [0,1,...,9]
+arr_slice = arr[5:8]
+# arr_slice = arr[5:8].copy()  # if you want to create a new object
+arr_slice[1] = 12345
+print(arr)   # Print  array[0,1,2,3,4,12,12345,12,8,9]
+
+# Two-dimensional arrays indexing
+arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr2d[2]) # array([7, 8, 9])
+print(arr2d[0][2])  # 3
+print(arr2d[0, 2]) # 3
+
+# multidimensional arrays indexing
+arr3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(arr3d[0])   # array([[1, 2, 3],
+                  #  [4, 5, 6]])
+
+arr3d[0] = 42
+print(arr3d)    # [[[42 42 42]
+                #  [42 42 42]]
+                # [[ 7  8  9]
+                #  [10 11 12]]]
+
+# arr3d[1, 0]gives you all of the values whose indices start with(1, 0),forming a 1-dimensional array:
+print(arr3d[1, 0]) # array([7, 8, 9])
+
+arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr2d[:2])   #[[1 2 3]
+                   # [4 5 6]]
+print(arr2d[:, 2])  # [3 6 9]
+print(arr2d[1, [2, 3]])  # [5,6]
+print(arr2d[:2, 1:])  # [[2 3]
+                      # [5 6]]
+
+
+# Integer array indexing
 a = np.array([[1, 2], [3, 4], [5, 6]])
 
 # An example of integer array indexing.
@@ -166,7 +229,90 @@ print(a[bool_idx])  # Prints "[3 4 5 6]"
 # We can do all of the above in a single concise statement:
 print(a[a > 2])     # Prints "[3 4 5 6]"
 
-# =========================== Datatypes ===========================
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+data = np.random.randn(7, 4)
+print(data[names == 'Bob'])
+print(data[names == 'Bob', 2:])
+print(data[~(names == 'Bob')])
+mask = (names == 'Bob') & (names == 'Will')
+print(data[mask])
+data[data < 0] = 0
+
+# ====================== Fancy Indexing ==========================
+# Fancy indexing is a term adopted by NumPy to describe indexing using integer arrays. Suppose we had a 8 × 4 array:
+
+arr = np.full((8, 4), 0)
+
+for i in range(8):
+    arr[i] = i
+
+print(arr[[4, 3, 0, 6]])
+print(arr[[-3, -5, -7], 0])
+print(arr[[1, 5, 7, 2], [0, 3, 1, 2]]) # selects pairwise points in the matrix
+print(arr[(1, 0), (2, 0)])
+print(arr[[1, 5, 7, 2]][[0, 3, 1, 2]])  # selected rows and columns
+
+
+# ==================== Transposing Arrays and Swapping Axes ==========
+arr = np.arange(15).reshape((3, 5))
+print(arr.T)
+arr = np.random.randn(6, 3)
+np.dot(arr.T, arr)
+arr = np.arange(32).reshape((2, 2, 4, 2))
+
+# ======================= Universal Functions: Fast Element-wise Array Functions =======================================
+
+"""A universal function, or ufunc, is a function that performs elementwise operations on data in ndarrays. You can 
+think of them as fast vectorized wrappers for simple functions that take one or more scalar values and produce one or 
+more scalar results."""
+
+arr = np.arange(10)
+np.sqrt(arr)
+np.exp(arr)
+
+x = np.random.randn(8)
+y = np.random.randn(8)
+print(np.maximum(x, y))
+
+
+arr = [1.5, 2, 3.1, -4.9]
+print(np.modf(arr))   # (array([ 0.5,  0. ,  0.1, -0.9]), array([ 1.,  2.,  3., -4.]))
+
+# Unary ufuncs
+print(np.abs(arr)) # Compute the absolute value element-wise for integer, floating point, or complex values.
+print(np.square(arr))
+print(np.sqrt(arr))
+print(np.exp(arr))
+print(np.log10([1, 2, 3]))  # log, log10, log2
+print(np.sign([1, -2, 3, 0]))  # [1, -1, 1, 0]
+print(np.ceil([1.4, -2.1, 3.1, 0.1]))  # [1, -1, 1, 0]
+print(np.floor([1.4, -2.1, 3.1, 0.1]))  # [1, -3, 3, 0]
+print(np.rint([1.4, -2.1, 3.1, 0.1]))  # [ 1. -2.  3.  0.]  # Round elements to the nearest integer, preserving the dtype
+print(np.modf([1.4, -2.1, 3.1, 0.1])) # Return fractional and integral parts of array as separate array
+print(np.isnan([1.4, np.nan, 3.1, 0.1])) # [False,True, False, False] # Return boolean array indicating whether each value is NaN (Not a Number)
+print(np.isfinite([np.inf, np.nan, 3.1, 0.1])) # Return boolean array indicating whether each element is finite (non-inf, non-NaN) or infinite, respectively
+print(np.cos([30, 60, 90, 180]))  # cos, cosh, sin, sinh, tan, tanh, arccos, arccosh, arcsin, arcsinh, arctan, arctanh
+print(np.logical_not([np.nan, 0, 90, 180]))  # cos, cosh, sin, sinh, tan, tanh, arccos, arccosh, arcsin, arcsinh, arctan, arctanh
+
+# Binary universal functions
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 0, 2])
+
+print(arr1+arr2)
+print(arr1-arr2)
+print(arr1*arr2)
+print(arr1/arr2)
+print(arr1**arr2)
+print(np.maximum(arr1, arr2))
+print(np.fmax(arr1, arr2)) #ignore np.nan
+print(np.minimum(arr1, arr2))
+print(np.fmin(arr1, arr2))
+print(np.mod(arr1, arr2))
+print([arr1 >= arr2])
+print(np.logical_or(arr1>0, arr2>0))
+print(np.logical_and(arr1>0, arr2>0))
+
+# ======================= Datatypes ===========================
 x = np.array([1, 2])   # Let numpy choose the datatype
 print(x.dtype)         # Prints "int64"
 
@@ -176,15 +322,25 @@ print(x.dtype)             # Prints "float64"
 x = np.array([1, 2], dtype=np.int64)   # Force a particular datatype
 print(x.dtype)                         # Prints "int64"
 
+a = np.array([1, 2, 3], dtype=np.float64)
+print(a.astype(np.int64))   # [1., 2. 3.]
+
+# when use astype, it is copied into a new object. Hence, assignment is required.
+numeric_strings = np.array(['1.25', '-9.6', '42'])
+numeric_strings = numeric_strings.astype(np.float64)   # [1.25, -9.6, 42]
+
 # =========================== Array math ===========================
-x = np.array([[1, 2],[3, 4]], dtype=np.float64)
-y = np.array([[5, 6],[7, 8]], dtype=np.float64)
+x = np.array([[1, 2], [3, 4]], dtype=np.float64)
+y = np.array([[5, 6], [7, 8]], dtype=np.float64)
 
 # Elementwise sum; both produce the array
 # [[ 6.0  8.0]
 #  [10.0 12.0]]
 print(x + y)
 print(np.add(x, y))
+
+# Inverse
+print(1/x + 1/y)
 
 # Elementwise difference; both produce the array
 # [[-4.0 -4.0]
